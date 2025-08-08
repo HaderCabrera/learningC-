d96663e589e35d49299c627ba003a1ab8283a2b4
HaderCabrera <hader.cabrera96@gmail.com>
2025-08-08 07:11:12 -0500

    Hola mundo dotnet run

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Console.WriteLine("Hello, World!");
+Console.WriteLine("hola");*/
+
+// Console.WriteLine("eTUPEFACTO");
+
+using System;
+
+namespace TourOfCsharp;
+
+class Program
+{
+    static void Main()
+    {
+        // This line prints "Hello, World" 
+        Console.WriteLine("Hello, World");
+        Console.WriteLine(typeof(string).Assembly.ImageRuntimeVersion);
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Aprendizaje de C#
 
+1. Intalacion de C#
+    1. [Instalar dotnet](https://dotnet.microsoft.com/en-us/download/).
+    1. Instalar la extension `C# Dev Kit` en VSC.
+    
+
 ## Aprendizaje de ASP.NET
 
-
+1. CURSOS:
+    1. [Intorduccion de .NET, Necesario C#](https://learn.microsoft.com/en-us/training/modules/dotnet-introduction/)
